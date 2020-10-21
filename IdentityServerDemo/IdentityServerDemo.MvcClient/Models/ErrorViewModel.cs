c20068e10a73ee5d5ea75549b70ed777c172f9b4
Amit P Naik <amit.naik8103@gmail.com>
2020-10-21 23:32:38 +0530

    refactor!: modifying to proper port number

--- IdentityServerDemo/IdentityServerDemo.MvcClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentityServerDemo.MvcClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- IdentityServerDemo/IdentityServerDemo.MvcClient/Models/Token.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IdentityServerDemo.MvcClient.Models
-{
-    public class Token
-    {
-        public string access_token { get; set; }
-    }
-}
